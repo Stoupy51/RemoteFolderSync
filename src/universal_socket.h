bfc04bb1bfc08795fe3a0221f5c64ccdba939773
Stoupy51 <stoupy51@gmail.com>
2023-07-30 00:00:57 +0200

    Universal socket update

--- src/universal_socket.h
+++ src/universal_socket.h
@@ -4,20 +4,23 @@
 
 #ifdef _WIN32
 	#include <winsock2.h>
-	#define socklen_t int
+	typedef int socklen_t;
+	typedef SOCKET socket_t;
 
 	#define socket_close(socket) closesocket(socket)
 #else
+	#include <unistd.h>
 	#include <sys/socket.h>
 	#include <netinet/in.h>
 	#include <arpa/inet.h>
-	#define SOCKET int
+	typedef int socket_t;
 	#define INVALID_SOCKET -1
 
 	#define socket_close(socket) close(socket)
 #endif
-#define socket_read(socket, buffer, size, flags) recv(socket, (char*)buffer, size, flags)
-#define socket_write(socket, buffer, size, flags) send(socket, (char*)buffer, size, flags)
+
+#define tcp_read(socket, buffer, size, flags) recv(socket, (char*)buffer, size, flags)
+#define tcp_write(socket, buffer, size, flags) send(socket, (char*)buffer, size, flags)
 
 #endif
 
